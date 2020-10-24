43507b891e347f39a941ba1fb9920966d3142d9f
cade <71267868+hwcadebrown@users.noreply.github.com>
2020-10-24 19:14:57 +0100

    Update REGISTERJS.js
    need to try and implement feedback on the text

--- main/front-end/javascript/REGISTERJS.js
+++ main/front-end/javascript/REGISTERJS.js
@@ -2,7 +2,6 @@ function showPass() {
   var x = document.getElementById("passwordtxt");
   if (x.type === "password") {
     x.type = "text";
-    document.body.style.color = "purple";
   } else {
     x.type = "password";
   }
