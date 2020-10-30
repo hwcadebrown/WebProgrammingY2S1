d43b3c6e48c3f8da71743c0e898cbb2c65137381
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-10-30 18:05:08 +0000

    The basic area code for the game stage. Has no walls and we need to go over and make it tile based.

--- docs/area.js
+++ docs/area.js
@@ -5,7 +5,17 @@ class MainArea {
   constructor(x,y){
     this.x = x;
     this.y =y;
-    this.main = this.createStage(this.x,this.y //Will call this method within the class in order to populatio
+    this.main = this.createStage(this.x,this.y) //Will call this method within the class in order to populatio
+    createStage(x,y){ //Creates the grid
+      let main = [];
+      for (let count = 0, count<x, count++) {
+        let noColumn = [];
+        for (let count2 = 0; count2<y; count2++) {
+          noColumn.push(1);
+        }
+        main.push(noColumn);
+      }
 
-  }
-}
+    };
+  };
+};
