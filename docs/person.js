dc71c30c56225095d7841b6494ba6b78fa9bcf23
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-10-30 12:55:45 +0000

    The people generator so far plus the Person Object.

---
+++ docs/person.js
@@ -0,0 +1,21 @@
+//Alexander Wickham
+function Person(x, y, personType) {
+  this.y = y;
+  this.x = x;
+  this.personType = personType;
+  this.setPointType = function(personType){
+    switch (personType) {
+      case 0:
+        break;
+      case 1;
+        break;
+      case 2;
+        break;
+      case 3;
+        break;
+      case 4;
+        break;
+    }
+  };
+};
+var aPerson = new Person(0,0,0);
