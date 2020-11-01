d37b7f9483cc4e4c58badb3cde84ea8d0f308c03
hwcadebrown <cb313@hw.ac.uk>
2020-11-01 22:29:51 +0000

    basic game functionality, needs lots of work, no graphics implemented

--- main/front-end/javascript/GAMEPLAY.js
+++ main/front-end/javascript/GAMEPLAY.js
@@ -1,51 +1,152 @@
-//Nicole Stewart
+const GAME_SPEED = 25;
+const arena = document.getElementById('area');
+$(arena).css('width', 1400);
+$(arena).css('height', 700);
 
-//create the canvas
-//var gameboard = document.getElementById("canvas");
-//var context = gameboard.getContext("2d");
-//set the canvas background colour
-//const canvasBackgroundColour="#3e3e3e";
-//context.fillStyle=canvasBackgroundColour;
-//context.fillRect(0, 0, gameboard.width, gameboard.height);
+let dx = 10;
+let dy = 0;
 
-//set the image size of the train
-//const image = new Image(60, 50);
-//draw the image onto the screen
-//image.onload = drawImage;
-//link to the image of the train
-//image.src = "../graphics/trains/BLUECARRIAGEFRONT.png";
+start();
 
-//function to draw the image onto the screen
-//function drawImage() {
-  //context.drawImage(this, 0, 0, this.width, this.height);
-//}
+function start() {
+    score = 0;
+		$('#score').html(0);
 
-window.onload=function(){
-    var x=0;
-    var y=0;
-    var key,pos=0;
-    var canvas=document.getElementById("canvas");
-    var ctx=canvas.getContext("2d");
-    const img=new Image(10, 10);
-    img.onload=function()
-    {
-      ctx.drawImage(img,x,y, 50, 50);
-    }
-    img.src="../graphics/trains/BLUECARRIAGEFRONT.png";
-    document.onkeydown=function(e)
-    {
-      pos++;
-      key=window.event?e.keyCode:e.which;
-    }
-    document.onkeyup=function(e){pos--}
-    setInterval(function()
-    {
-      if(pos==0)return;
-      if(key==37)x-=1;
-      if(key==38)y-=1;
-      if(key==39)x+=1;
-      if(key==40)y+=1;
-      canvas.width=canvas.width;
-      ctx.drawImage(img,x,y, 50, 50);
+		train = [ {x: 750, y: 350}, {x: 740, y: 350}, {x: 730, y: 350} ];
+
+		$('#area').empty();
+
+		status();
+		createperson();
+    addperson();
+
+		$(document).on("keydown", changedirection);
+}
+
+function status() {
+  if(playerHitWall()) {
+    alert('You Derailed!');
+    start();
+    return;
+  }
+
+  setTimeout(function onTick() {
+    changingDirection = false;
+    status();
+    renderperson();
+    rendertrain();
+    advancetrain();
+  }, GAME_SPEED)
+}
+
+function addperson() {
+	  let person = document.createElement('person');
+		person.id = "person";
+		$(person).css('width', 10);
+    $(person).css('height', 10);
+	  $(person).css('position', 'absolute');
+		$(person).css('backgroundColor', 'gray');
+    $(person).appendTo(arena);
+
+		for (let n=0; n<train.length; n++)
+		{ addcarriage(n); }
+}
+
+function addcarriage(id) {
+		let carriage = document.createElement('carriage');
+		carriage.id = "carriage" + id;
+		$(carriage).css('width', 10);
+    $(carriage).css('height', 10);
+    $(carriage).css('position', 'absolute');
+		$(carriage).css('backgroundColor', '#0086aa');
+		$(carriage).appendTo(arena);
+	}
+
+function personpos(min, max) {
+    return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+}
+
+function createperson() {
+    personX = personpos(0, parseFloat($(arena).css('width')) - 10);
+    personY = personpos(0, parseFloat($(arena).css('height')) - 10);
+
+    train.forEach(function ispersonOntrain(part) {
+      const personIsoNtrain = part.x.personX && part.y.personY;
+      if (personIsoNtrain) createperson();
     });
+}
+
+function renderperson() {
+	  $(person).css('left', personX);
+	  $(person).css('top', personY);
+}
+
+function advancetrain() {
+    const head = {x: train[0].x + dx, y: train[0].y + dy};
+    train.unshift(head);
+
+    const didPickUpPerson = train[0].x === personX && train[0].y === personY;
+    if (didPickUpPerson) {
+      score++;
+      $('#score').html(score);
+
+		addcarriage(train.length - 1);
+      createperson();
+    }
+    else {
+      train.pop();
+    }
+}
+
+function rendertrain() {
+	  for (let t=0; t < train.length; t++)
+	  { rendertrainPart(t, train[t]) }
+}
+
+function rendertrainPart(t, trainPart) {
+	  let part = "carriage" + t;
+		let carriage = document.getElementById(part);
+		$(carriage).css('left', trainPart.x);
+		$(carriage).css('top', trainPart.y);
+}
+
+function changedirection(event) {
+    const direction = event.keyCode;
+
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+    const W = 87;
+    const A = 65;
+    const S = 83;
+    const D = 68;
+
+    const moveUp = dy  === -10;
+    const moveDown = dy  ===  10;
+    const moveRight = dx  ===  10;
+    const moveLeft = dx  === -10;
+
+      if (direction === LEFT_KEY && !moveRight || direction === A && !moveRight) {
+        dx = -10;
+        dy =  0;
+      } else if (direction === UP_KEY && !moveDown || direction === W && !moveDown) {
+        dx =  0;
+        dy = -10;
+      } else if (direction === RIGHT_KEY && !moveLeft || direction === D && !moveLeft) {
+        dx = 10;
+        dy = 0;
+      } else if (direction === DOWN_KEY && !moveUp || direction === S && !moveUp) {
+        dx = 0;
+        dy = 10;
+      }
+}
+
+function playerHitWall() {
+    const hitLeftWall = train[0].x < 0;
+    const hitRightWall = train[0].x > parseFloat(arena.style.width) - 10;
+    const hitTopWall = train[0].y < 0;
+    const hitBottomWall = train[0].y > parseFloat(arena.style.height) - 10;
+
+    return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
 }
