acb0773bdc5aafd70133831ea0f87effcc5d2f01
hwcadebrown <cb313@hw.ac.uk>
2020-11-10 13:21:43 +0000

    updated docs, should tidy up at some point this week

---
+++ docs/client.js
@@ -0,0 +1,51 @@
+function randompos(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 50) * 50;
+}
+
+function changedirection(event) {
+  const direction = event.keyCode;
+
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+  const W = 87;
+  const A = 65;
+  const S = 83;
+  const D = 68;
+
+  const moveUp = directiony === -50;
+  const moveDown = directiony === 50;
+  const moveRight = directionx === 50;
+  const moveLeft = directionx === -50;
+
+  if (direction === LEFT_KEY && !moveRight || direction === A && !moveRight) {
+    directionx = -50;
+    directiony = 0;
+  } else if (direction === UP_KEY && !moveDown || direction === W && !moveDown) {
+    directionx = 0;
+    directiony = -50;
+  } else if (direction === RIGHT_KEY && !moveLeft || direction === D && !moveLeft) {
+    directionx = 50;
+    directiony = 0;
+  } else if (direction === DOWN_KEY && !moveUp || direction === S && !moveUp) {
+    directionx = 0;
+    directiony = 50;
+  }
+}
+
+function playerHitSelf() {
+  for (let cart = 3; cart < train.length; cart++) {
+    if (train[cart].x === train[0].x && train[cart].y === train[0].y) return true;
+  }
+}
+
+function playerHitWall() {
+
+  const hitLeftWall = train[0].x < 0;
+  const hitRightWall = train[0].x > parseFloat($(arena).css('width')) - 50;
+  const hitTopWall = train[0].y < 0;
+  const hitBottomWall = train[0].y > parseFloat($(arena).css('height')) - 50;
+
+  return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
+}
---
+++ docs/people.js
@@ -0,0 +1,34 @@
+function addperson() {
+  let person = document.createElement('person');
+  person.id = "person";
+  $(person).css('width', 50);
+  $(person).css('height', 50);
+  $(person).css('position', 'absolute');
+  $(person).css('background-image', 'url(' + 'graphics/gifs/PERSON1.gif' + ')');
+  $(person).appendTo(arena);
+
+  let person2 = document.createElement('person2');
+  person2.id = "person2";
+  $(person2).css('width', 50);
+  $(person2).css('height', 50);
+  $(person2).css('position', 'absolute');
+  $(person2).css('background-image', 'url(' + 'graphics/gifs/PERSON2.gif' + ')');
+  $(person2).appendTo(arena);
+}
+
+function createperson() {
+  personX = randompos(0, parseFloat($(arena).css('width')) - 50);
+  personY = randompos(0, parseFloat($(arena).css('height')) - 50);
+}
+
+function createperson2() {
+  personX2 = randompos(0, parseFloat($(arena).css('width')) - 50);
+  personY2 = randompos(0, parseFloat($(arena).css('height')) - 50);
+}
+
+function renderperson() {
+  $(person).css('left', personX);
+  $(person).css('top', personY);
+  $(person2).css('left', personX2);
+  $(person2).css('top', personY2);
+}
