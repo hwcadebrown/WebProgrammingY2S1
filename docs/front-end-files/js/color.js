67a1525c75e76e9ed4482bf6e0ba4fda77b69120
hwcadebrown <cb313@hw.ac.uk>
2020-11-11 23:24:20 +0000

    doc page updates to recent version of game :]

---
+++ docs/front-end-files/js/color.js
@@ -0,0 +1,33 @@
+function colorPicker() {
+  var color = Math.floor((Math.random() * 10) + 1);
+  if(color === 1) {
+    return '../../graphics/trains/PINKTRAIN.png';
+  }
+  if(color === 2) {
+    return '../../graphics/trains/BLUETRAIN.png';
+  }
+  if(color === 3) {
+    return '../../graphics/trains/GREENTRAIN.png';
+  }
+  if(color === 4) {
+    return '../../graphics/trains/REDTRAIN.png';
+  }
+  if(color === 5) {
+    return '../../graphics/trains/ORANGETRAIN.png';
+  }
+  if(color === 6) {
+    return '../../graphics/trains/SKYBLUETRAIN.png';
+  }
+  if(color === 7) {
+    return '../../graphics/trains/DARKGREENTRAIN.png';
+  }
+  if(color === 8) {
+    return '../../graphics/trains/PURPLETRAIN.png';
+  }
+  if(color === 9) {
+    return '../../graphics/trains/PEACHTRAIN.png';
+  }
+  if(color === 10) {
+    return '../../graphics/trains/GRAYTRAIN.png';
+  }
+}
