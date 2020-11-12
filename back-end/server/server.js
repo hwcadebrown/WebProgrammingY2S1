cf72d31afb24760813fe42124786fed416f0dd0f
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-11-12 11:52:10 +0000

    Added some basic changes to it so it works properly and the port is designed properly.

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -1,10 +1,12 @@
 //Alexander Wickham
-const socketio = require("socketio")
+const socketio = require("socketio");
+const express = require("express");
+let tempPort = process.env.PORT || 3000;
 let app;
-var tempPort
 try {
   app = require("./app")
 }
 catch(error){
   console.log(error)
 }
+const server = app.listen(port, () => console.log(`Listening on log ${port}...`));
