7f0ecb71efdfbf83fb968529d7b6df613cd4c6cd
hwcadebrown <cb313@hw.ac.uk>
2020-11-16 23:16:08 +0000

    comments done for PLAY.js and TRAIN.js might be changed a little later

--- front-end/javascript/PLAY.js
+++ front-end/javascript/PLAY.js
@@ -1,76 +1,119 @@
+// Cade Brown & Glenn Wright
+// Base of game, mostly where functions are called and game is started etc.
+
+// determines the speed of the game, lower = faster & higher = slower
 const GAME_SPEED = 50;
+
+// the arena on which the game is played on
+// assigns arena to the area element from PLAY.html
 const arena = document.getElementById('area');
+
+// styles the arena's width and height
 $(arena).css('width', 4750);
 $(arena).css('height', 2400);
 
+/* directionx and directiony intitialised to placeholder values,
+the directionx of 50 and directiony of 0 will start the player
+moving right, these determine the direction the player is moving
+in at a specific instance */
 let directionx = 50;
 let directiony = 0;
+
+// calls the start function, which intitialises the elements needed
 start();
 
+// start function which will create what we want/need for gameplay
 function start() {
+
+  // the score counter, starts at 0 and uses the element from PLAY.html
   $('#score').html(score = 0);
 
+  /* creates the first carriage on our train, starting at the length
+  of 1, the spawn point of the train is randomly assigned to a place
+  in the arena */
   train = [{
     x: randompos(0, parseFloat($(arena).css('width')) - 50),
     y: randompos(0, parseFloat($(arena).css('height')) - 50)
   }];
 
-	$('#area').empty();
+  /* the arena is cleared of any sprites that were visible on previous
+  playthrough */
+  $('#area').empty();
 
+  // calls the status function which is used to advance the gameplay
   status();
-  //playerHits();
+
+  /* creates the elements we need for gameplay, this includes the people
+  and adding them to the arena and the carriage for the players train */
   createperson();
   createperson2();
   createperson3();
   addperson();
   attatchcarriage();
 
+  // calls the changedirection function whenever the player pressed a key
   $(document).on("keydown", changedirection);
 }
-
 
 var gameOverBox;
 var gameState;
 
-
-
 gameOverBox = document.getElementById("gameOver");
 
+// status function which advances the gameplay with repeated calls
 function status() {
+
+  /* if the player hits themselves or one of the arenas walls then
+  the game ends */
   if (playerHitWall() || playerHitSelf()) {
+
     // window.location.href='MAINMENU.html' (leave for now)
-    //alert('You Derailed! Score: ' + score);
+    // alert('You Derailed! Score: ' + score);
+
+    // sets the state of the setState function to gameOver
     setState("gameOver");
+
+    // restarts the game from the beginning by calling start function
     start();
     return;
   }
 
+  // if the gameState is not set to gameOver the game will advance
+  if (gameState != "gameOver") {
 
+    // game advances after each tick of the game
+    setTimeout(function onTick() {
+      // calls the status function again
+      status();
+      // playerHits();
+      // renders the people gifs and places them at a random position
+      renderperson();
+      // renders the train in the arena
+      rendertrain();
+      // advances the train according the the direction is heading in
+      advancetrain();
+    }, GAME_SPEED) // sets the GAME_SPEED to 50, if not set again would go supersonic speeds
 
-if (gameState != "gameOver") {
-  setTimeout(function onTick() {
-    changingDirection = false;
-    status();
-    //playerHits();
-    renderperson();
-    rendertrain();
-    advancetrain();
-
-  }, GAME_SPEED )
-}
-}
-  function setState(state) {
-
-    gameState = state;
-    showMenu(state);
   }
+}
 
-  function displayMenu(menu) {
-    menu.style.visibility = "visible";
-  }
+// setts the state of the game
+function setState(state) {
 
-  function showMenu(state) {
-  if(state == "gameOver") {
+  // sets the gameState to the value of the parameter state
+  gameState = state;
+  // calls the showMneu function with the current state
+  showMenu(gameState);
+}
+
+// makes the gameOver menu visible to the player
+function displayMenu(menu) {
+  menu.style.visibility = "visible";
+}
+
+// shows the gameOver menu to the player
+function showMenu(state) {
+  if (state == "gameOver") {
     displayMenu(gameOverBox);
   }
 }
--- front-end/javascript/TRAIN.js
+++ front-end/javascript/TRAIN.js
@@ -1,57 +1,104 @@
+// Cade Brown
+// Trains functions for the gameplay
+
+// function which adds a carriage to the game
 function addcarriage(id) {
+  // carriage variable initialised with identifier 'carriage'
   let carriage = document.createElement('carriage');
   carriage.id = "carriage" + id;
+  // width of the carriage set to 50px
   $(carriage).css('width', 50);
+  // height of the carriage set to 50px
   $(carriage).css('height', 50);
+  // the position of the carriage will be absolute, placed with randompos
   $(carriage).css('position', 'absolute');
+  // sets the color of the carriage, this uses the colorPicker function
   $(carriage).css('background-image', 'url(' + colorPicker() + ')');
+  // adds the carriage to the games arena
   $(carriage).appendTo(arena);
 }
 
+// attacthes the carriage to the train
 function attatchcarriage() {
+  // amount of carriages added won't go larger than the whole length of train
   for (let i = 0; i < train.length; i++) {
+    // calls the addcarriage function to add to the train
     addcarriage(i);
   }
 }
 
+// this function will advance the train and check collisions with people
 function advancetrain() {
+  /* creates the head of the train, this changes depending on what direction
+  the player is moving in */
   const head = {
     x: train[0].x + directionx,
     y: train[0].y + directiony
   };
+
+  /* this will add the head of the train to the beginning of the trains body,
+  and also allows the train to move from it's spawn position */
   train.unshift(head);
 
+  // variables which check if the trains head is at the position of the person
   const didPickUpPerson = train[0].x === personX && train[0].y === personY;
   const didPickUpPerson2 = train[0].x === personX2 && train[0].y === personY2;
   const didPickUpPerson3 = train[0].x === personX3 && train[0].y === personY3;
+
+  // if PERSON1 is picked up then this set of operations will be executed
   if (didPickUpPerson) {
+    // increments the score variable and adds one point
     $('#score').html(score += 1);
-
+    // adds a carriage at the ends of the trains body (last carriage added)
     addcarriage(train.length - 1);
+    // creates a new PERSON1 in the arena to be collected again
     createperson();
-  } else if (didPickUpPerson2) {
-    $('#score').html(score += 2);
 
+    // if PERSON2 is picked up then this set of operations will be executed
+  } else if (didPickUpPerson2) {
+    // adds 2 points to the score variable
+    $('#score').html(score += 2);
+    // adds a carriage at the ends of the trains body (last carriage added)
     addcarriage(train.length - 1);
+    // creates a new PERSON2 in the arena to be collected again
     createperson2();
+
+    // if PERSON3 is picked up then this set of operations will be executed
   } else if (didPickUpPerson3) {
+    // takes 1 point off the score variable
     $('#score').html(score -= 1);
+    // creates a new PERSON3 in the arena to be collected again
     createperson3();
+    /* removes the last carriage of the train, if this isn't put in then an
+    invisble carriage will be attatched so this will prevent it */
     train.pop();
   } else {
+    /* else case removes the last carriage of the train, if this isn't done
+    then a ghost train of the player will spawn re-enacting all the movements
+    the player makes and if the player hits any previously visited position
+    the player dies, truly scary stuff */
     train.pop();
   }
 }
 
+// renders the train on the arena to be visible to the player
 function rendertrain() {
+  /* loops through each carriage attatched to the train and will render
+  each carriage that the player currently has on their train */
   for (let t = 0; t < train.length; t++) {
+    // calls the rendertrainPart function to render each part
     rendertrainPart(t, train[t])
   }
 }
 
+// renders individual parts to be seen on the arena
 function rendertrainPart(t, trainPart) {
+  // renders the carriage at the position t of the train
   let part = "carriage" + t;
+  // assigns the carriage variable to equal the part variable we just created
   let carriage = document.getElementById(part);
+  // assigns the left property to the x-coord of the train
   $(carriage).css('left', trainPart.x);
+  // assigns the top property to the y-coord of the train
   $(carriage).css('top', trainPart.y);
 }
