0f9dca141f388ba4b844a3821d8b4e610b7cc8bc
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-11-17 16:32:57 +0000

    Added the basic socket to begin with

---
+++ back-end/Sockets/PLAY.js
@@ -0,0 +1,13 @@
+const Passenger = require("./Properties/PEOPLE");
+const Area = require("./Properties/AREA");
+const Train = require("./Properties/TRAIN");
+class Play {
+  constructor(){}
+  trainUpdates(){}
+  addNewPlayer(){}
+  removePlayer(){}
+  changeDirection(){}
+}
+modules.export = {
+  Play
+};
--- back-end/server/server.js
+++ back-end/server/server.js
@@ -17,7 +17,7 @@ catch(error){
   console.log(error)
 }
 //Creates a server with a listen and a port
-const server = app.listen(port, () => console.log(`Listening on log ${port}...`));
+const server = app.listen(tempPort, () => console.log(`Listening on log ${temPort}...`))
 //Makes it so the server can be use socket module properly
 const io = socketio(server);
 //Uses the main play file a requirement as it nessarcy to use it
@@ -26,7 +26,7 @@ const game_play = require("./Socket/PLAY")
 let gameCurrent = new game_play.Play;
 //Will stream the game to the user
 let Run_Game = () =>
-  gameCurrent.trainUpdates());
+  gameCurrent.trainUpdates())
   io.socket.emit('game situation', gameCurrent);
 }
 //sets a base connection to the server that starts the game off.
@@ -34,17 +34,17 @@ io.on('connection',function(newPlayer){
   //Adds a new player to the game, will change it so it incorperates the player name into train
   gameCurrent.addNewPlayer(newPlayer);
   //Runs the game for the user
-  Run_Game();
+  Run_Game()
   //Creates a bunch of listener that will activate once the user trigger an event to create them
   //If the user changes direction it
   newPlayer.on('changesDirection',function(direction){
 
-  });
+  })
 
 
   newPlayer.on('pickUpPassenger', function(person){
 
-  });
+  })
   //May need another one
 
   //newPlayer.on('',function(){});
@@ -52,5 +52,8 @@ io.on('connection',function(newPlayer){
   //Listens out for the disconnect after the user leave for one reason or another
   newPlayer.on('disconnect',function(){
     console.log('user has  left the game')
-  });
-});
+  })
+})
+setInterval(function(){
+
+}, 200)
