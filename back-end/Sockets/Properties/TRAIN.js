bca8395975c8eecf273a768a0d4c9e6c9e4bb3eb
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-11-18 21:26:02 +0000

    I need to add changes to Area js in order to make sure the train system to work

--- back-end/Sockets/Properties/TRAIN.js
+++ back-end/Sockets/Properties/TRAIN.js
@@ -1,5 +1,46 @@
+const Area = require("./AREA");
+
 class Train{
-  constructor(){}
+  constructor(x,y, uniqueID){
+    this.id = uniqueID;
+    this.x = x;
+    this.y = y;
+    this.direction = {leftOrright:0, upOrdown:1};
+    this.length = 1;
+    this.trainColour = 0;
+    this.noCarriages = [x,y];
+    this.isActive = true;
+  }
+
+  updateTrain(area){
+    this.x += this.direction.leftOrright
+    this.y += this.direction.upOrdown
+
+    let carriages = area.infoAtTile(this.x,this.y);
+
+    this.collisionDetect(area, carriages)
+
+    if (this.isActive){
+      area = this.moveTrain;
+    }
+
+    return area;
+  }
+
+  collisionDetect(area, carriages){}
+    switch (expression) {
+      case expression:
+        
+        break;
+      default:
+
+    }
+  moveTrain(area){}
+
+  changeDirection(){}
+
+
+
 }
 module.exports = {
   Train
