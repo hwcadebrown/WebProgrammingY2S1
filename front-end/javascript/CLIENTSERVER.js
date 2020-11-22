68e22f7c2e9ee5732d4a894dd4871003fbff0381
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-11-22 01:25:32 +0000

    Most of the sockets are completed. Now we need to go add commentary, make server.js respond properly and make sure ClinetServer.js aligns with the front end.

---
+++ front-end/javascript/CLIENTSERVER.js
@@ -0,0 +1,20 @@
+//Alexander Wickham
+var webConnection = io();
+var train;
+var passengers;
+var area;
+var user;
+//This will call the server and user the Play class to create the setting nessarcy, and if any other commands are called, they they will activate through
+webConnection.on('game situation', function(play){
+  passengers = play.passengers;
+  train = play.trains;
+  area = play.area;
+  player = train.find(function(train){
+    return train.id == webConnection.id;
+  })||
+
+  if (player.isActive == False){
+
+    return;
+  }
+})
