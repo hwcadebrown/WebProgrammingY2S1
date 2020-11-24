c0b522f67874ac9522781ccca00e4c8790a8bcaa
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-11-24 13:11:33 +0000

    Added the correct disconnection properly.

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -29,6 +29,7 @@ let Run_Game = () =>
   gameCurrent.trainUpdates())
   io.socket.emit('game situation', gameCurrent);
 }
+
 //sets a base connection to the server that starts the game off.
 io.on('connection',function(newPlayer){
   //Adds a new player to the game, will change it so it incorperates the player name into train
@@ -40,9 +41,11 @@ io.on('connection',function(newPlayer){
   newPlayer.on('changesDirection',function(direction){
     gameCurrent.changeDirection(newPlayer, direction);
   })
-  
+
   newPlayer.on('disconnect',function(){
     console.log('user has  left the game')
+    gameCurrent.removePlayer(newPlayer)
+    Run_Game()
   })
 })
 
