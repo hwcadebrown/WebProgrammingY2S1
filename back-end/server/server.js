b47aeed753c365abae016f20f5b3976feb585fb9
hwcadebrown <cb313@hw.ac.uk>
2020-11-25 13:13:22 +0000

    serverjs peer check

--- back-end/server/server.js
+++ back-end/server/server.js
@@ -2,7 +2,6 @@
 //Adds the two modules required to run the server
 const socketio = require("socket.io");
 const express = require("express");
-
 //Creates a port a variable to gain information from the data in order to complete transactions nessarcy
 //Tempory until we get some sort of domain
 
@@ -13,42 +12,42 @@ const io = socketio();
 //Trys to find the app.js file otherwise it responds with an error
 try {
   app = require("./app")
-}
-catch(error){
+} catch (error) {
   console.log(error)
 }
 //Creates a server with a listen and a port
-const server = app.listen(tempPort, () => console.log(`Listening on log ${temPort}...`))
+const server = app.listen(tempPort, () => console.log(`Listening on log ${tempPort}...`))
 //Makes it so the server can be use socket module properly
 const io = socketio(server);
 //Uses the main play file a requirement as it nessarcy to use it
-const game_play = require("./Socket/PLAY")
+const game_play = require("../Sockets/PLAY") // NOTE: CADE CHANGED THIS
 //genreates a class of Play in order to stream it.
 let gameCurrent = new game_play.Play;
 //Will stream the game to the user
 let Run_Game = () =>
   gameCurrent.trainUpdates())
-  io.socket.emit('game situation', gameCurrent);
+io.socket.emit('game situation', gameCurrent);
 }
 
 //sets a base connection to the server that starts the game off.
-io.on('connection',function(newPlayer){
+io.on('connection', function(newPlayer) {
   //Adds a new player to the game, will change it so it incorperates the player name into train
   gameCurrent.addNewPlayer(newPlayer);
   //Runs the game for the user
   Run_Game()
   //Creates a bunch of listener that will activate once the user trigger an event to create them
   //If the user changes direction it
-  newPlayer.on('changesDirection',function(directionx, directiony){
-    gameCurrent.changeDirection(newPlayer, directionx, directiony);
+  newPlayer.on('changedirection', function(directionx, directiony) {
+    gameCurrent.changedirection(newPlayer, directionx, directiony);
   })
 
-  newPlayer.on('disconnect',function(){
-    console.log('user has  left the game')
+  newPlayer.on('disconnect', function() {
+    console.log('user has left the game')
     gameCurrent.removePlayer(newPlayer);
     Run_Game()
   })
 })
-setInterval(function(){
+
+setInterval(function() {
   Run_Game();
 }, 200)
