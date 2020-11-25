280113ecb90e82b5783852e426689da595d221f8
hwcadebrown <cb313@hw.ac.uk>
2020-11-25 16:17:11 +0000

    fix

--- back-end/Sockets/PLAY.js
+++ back-end/Sockets/PLAY.js
@@ -1,6 +1,6 @@
-const Passenger = require("./Properties/Passenger");
-const Area = require("./Properties/Area");
-const Train = require("./Properties/Train");
+const Passenger = require("Properties/Passenger");
+const Area = require("Properties/Area");
+const Train = require("Properties/Train");
 class Play {
   constructor() {
     this.gameStage = new Area.Area(98,48);
