9bc311d94af05c2b8f4418a5229cf6dddd29d9d1
Alexander-Wickham <71259927+Alexander-Wickham@users.noreply.github.com>
2020-11-25 22:05:42 +0000

    Trying to commit

--- front-end/javascript/CLIENTSERVER.js
+++ front-end/javascript/CLIENTSERVER.js
@@ -10,39 +10,95 @@ webConnection.on('game situation', function(play){
   passengers = play.passengers;
   allTrains = play.trains;
   area = play.area;
-  player = trains.find(function(train){
-    return allTrains.id == webConnection.id;
-  })
+  player = allTrains.find(function(player){
+    return player.id == webConnection.id;
+  }) || player;
 
-  if (player.isActive == False){
-    webConnection.on('')
+  if (!player.isActive){
+    webConnection.on('disconnect')
     return;
   }
+
+  const rerender = (millisec) => {
+    if (millis - checkPoint> 30){
+      for (int count = 0; count = allTrains.length; count++){
+        displayTrain(allTrains[count]);
+        displayPassengers();
+      }
+      checkPoint += 30;
+    }
+  };
+//This is the constant rendering that will take place.
+  rerender();
 })
 
+// function which adds the people to the game arena
+displayPeople() {
+  // first person variable initialised with identifier 'person'
+  var person = document.createElement('person');
+  // width of the person gif set to 50px
+  $(person).css('width', 50);
+  // height of the person gif set to 50px
+  $(person).css('height', 50);
+  // the position of the person will be absolute, placed with randompos
+  $(person).css('position', 'absolute');
+  // sets the background of the person to the specified person gif
+  $(person).css('background-image', 'url(' + '../graphics/gifs/PERSON1.gif' + ')');
+  // adds the person to the games arena
+  $(person).appendTo(arena);
+
+  // spower person variable initialised with identifier 'powerperson'
+  var powerperson = document.createElement('powerperson');
+  // width of the powerperson gif set to 50px
+  $(powerperson).css('width', 50);
+  // height of the powerperson gif set to 50px
+  $(powerperson).css('height', 50);
+  // the position of the powerperson will be absolute, placed with randompos
+  $(powerperson).css('position', 'absolute');
+  // sets the background of the powerperson to the specified person gif
+  $(powerperson).css('background-image', 'url(' + '../graphics/gifs/POWERPERSON.gif' + ')');
+  // adds the powerperson to the games arena
+  $(powerperson).appendTo(arena);
+
+  // bad person variable initialised with identifier 'badperson'
+  var badperson = document.createElement('badperson');
+  // width of the badperson gif set to 50px
+  $(badperson).css('width', 50);
+  // height of the badperson gif set to 50px
+  $(badperson).css('height', 50);
+  // the position of the badperson will be absolute, placed with randompos
+  $(badperson).css('position', 'absolute');
+  // sets the background of the badperson to the specified person gif
+  $(badperson).css('background-image', 'url(' + '../graphics/gifs/BADPERSON.gif' + ')');
+  // adds the badperson to the games arena
+  $(badperson).appendTo(arena);
+}
+
 displayTrain(train) {
-  if (this.isActive == False;) // train has crashed so do not display
+  if (!this.isActive;) // train has crashed so do not display
   {return;}
 
-  // draw train carriages
-  this.displaycarriages();
-
-  // iterates through each point on the train excluding the ends
-  for (var t = 0; t < train.length; t++) {
-    let cartPos = this.train[t]; // get current point
-    let cartPrevPos = this.train[t - 1]; // get point before (closer to head)
-    let cartNextPos = this.train[t + 1]; // get point after (closer to tail)
-
+  // iterates through each point on the train
+  for (var t = 0; t < train.length; t++){
+    // get current point
+    var x = train.carriages[t].leftOrright;
+    var y = train.carriages[t].upOrdown;
     // calculate grid position for plotting current point
-    let X = getPlotX(point[0]) + currentOffset.x;
-    let Y = getPlotY(point[1]) + currentOffset.y;
+    x = getFrontAreaPoint(x);
+    y = getFrontAreaPoint(y);
 
-    // get the image of the shape
-    let cartColor = colorPicker();
+   var carriage = document.createElement('carriage');
 
-    // draw the image at the appropriate angle
-    cartColor.display(X * TILE_SIZE, Y * TILE_SIZE); // draws cell
+   $(carriage.x).css(x, 50);
+   // height of the carriage set to 50px
+   $(carriage.y).css(y, 50);
+   // the position of the carriage will be absolute, placed with randompos
+   $(carriage).css('position', 'absolute');
+   // sets the color of the carriage, this uses the colorPicker function
+   $(carriage).css('background-image', 'url(' + colorPicker() + ')');
+   // adds the carriage to the games arena
   }
-  // draw train head
-  this.rendertrain();
+}
+getFrontAreaPoint(gridSpot){
+  return (gridSpot*50);
 }
