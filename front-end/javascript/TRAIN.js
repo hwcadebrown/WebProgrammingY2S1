dc57a281b025b70e8ff0ff82de0f6113bb4f5120
hwcadebrown <cb313@hw.ac.uk>
2020-11-25 20:12:33 +0000

    FRICKING FRONT END
    HWG

--- front-end/javascript/TRAIN.js
+++ front-end/javascript/TRAIN.js
@@ -1,3 +1,89 @@
+// Cade Brown
+// Trains functions for the gameplay
+
+// function which adds a carriage to the game
+function addcarriage(id) {
+  // carriage variable initialised with identifier 'carriage'
+  var carriage = document.createElement('carriage');
+  carriage.id = "carriage" + id;
+  // width of the carriage set to 50px
+  $(carriage).css('width', 50);
+  // height of the carriage set to 50px
+  $(carriage).css('height', 50);
+  // the position of the carriage will be absolute, placed with randompos
+  $(carriage).css('position', 'absolute');
+  // sets the color of the carriage, this uses the colorPicker function
+  $(carriage).css('background-image', 'url(' + colorPicker() + ')');
+  // adds the carriage to the games arena
+  $(carriage).appendTo(arena);
+}
+
+// attacthes the carriage to the train
+function attatchcarriage() {
+  // amount of carriages added won't go larger than the whole length of train
+  for (var i = 0; i < train.length; i++) {
+    // calls the addcarriage function to add to the train
+    addcarriage(i);
+  }
+}
+
+// this function will advance the train and check collisions with people
+function advancetrain() {
+  /* creates the head of the train, this changes depending on what direction
+  the player is moving in */
+  const head = {
+    x: train[0].x + directionx,
+    y: train[0].y + directiony
+  };
+
+  /* this will add the head of the train to the beginning of the trains body,
+  and also allows the train to move from it's spawn position */
+  train.unshift(head);
+
+  // variables which check if the trains head is at the position of the person
+  const didPickUpPerson = train[0].x === personX && train[0].y === personY;
+  const didPickUpPowerPerson = train[0].x === personXpower && train[0].y === personYpower;
+  const didPickUpBadPerson = train[0].x === personXbad && train[0].y === personYbad;
+
+  // if PERSON1 is picked up then this set of operations will be executed
+  if (didPickUpPerson) {
+    // increments the score variable and adds one point
+    $('#score').html(score += 1);
+    // adds a carriage at the ends of the trains body (last carriage added)
+    addcarriage(train.length - 1);
+    // creates a new PERSON1 in the arena to be collected again
+    createperson(1);
+
+    // if PERSON2 is picked up then this set of operations will be executed
+  } else if (didPickUpPowerPerson) {
+    // adds 2 points to the score variable
+    $('#score').html(score += 2);
+    // adds a carriage at the ends of the trains body (last carriage added)
+    addcarriage(train.length - 1);
+    // creates a new PERSON2 in the arena to be collected again
+    createperson(2);
+
+    // if PERSON3 is picked up then this set of operations will be executed
+  } else if (didPickUpBadPerson) {
+    // takes 1 point off the score variable
+    $('#score').html(score -= 1);
+    if(score < 0) {
+      $('#score').html(score = 0);
+    }
+    // creates a new PERSON3 in the arena to be collected again
+    createperson(3);
+    /* removes the last carriage of the train, if this isn't put in then an
+    invisble carriage will be attatched so this will prevent it */
+    train.pop();
+  } else {
+    /* else case removes the last carriage of the train, if this isn't done
+    then a ghost train of the player will spawn re-enacting all the movements
+    the player makes and if the player hits any previously visited position
+    the player dies, truly scary stuff*/
+    train.pop();
+  }
+}
+
 // renders the train on the arena to be visible to the player
 function rendertrain() {
   /* loops through each carriage attatched to the train and will render
